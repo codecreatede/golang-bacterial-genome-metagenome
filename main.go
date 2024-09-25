6d87e8706c1a3b2ed810c1b8cffa26a5a4d0a126
Gaurav Sablok <gauravcodepro@gmail.com>
2024-09-25 22:47:40 +0200

    codecreatede
    started writing the code for this application and this will analyze all the bacterial and metagenomes.

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+
+Author Gaurav Sablok
+Universitat Potsdam
+Date 2024-9-26
+
+A golang microbiome and metagenomics data analyzer. It analyzes the microbial genomes either through the
+typed annotations or generates the annotation as required by the NCBI microbial consortium submissions.
+Typed annotation means desining the microbial tags for the sequencing. It takes the genome and allows
+you to generate and extract the tags for the microbial genomes.
+
+*/
